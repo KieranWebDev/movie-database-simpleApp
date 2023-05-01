2e20651156f6cb57e80dbcf1ef508a11ac95dfd6
Kieran <kieran.webdevelopment@gmail.com>
2023-05-01 14:59:40 +0100

    nav and search bar css complete

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // const apiKey = '10594f76';
 
-async function getData() {
-  const response = await fetch(
-    `http://www.omdbapi.com/?s=star wars&apikey=10594f76`
-  );
-  const data = await response.json();
-  console.log(data);
-}
-await getData();
+// async function getData() {
+//   const response = await fetch(
+//     `http://www.omdbapi.com/?s=star wars&apikey=10594f76`
+//   );
+//   const data = await response.json();
+//   console.log(data);
+// }
+// await getData();
